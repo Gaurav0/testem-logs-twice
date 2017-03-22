aa2249a9e42cead0c0e6d4fb556286d3302a843c
Gaurav Munjal <gmunjal@linkedin.com>
2017-03-22 10:08:07 -0700

    Show the problem

--- testem.js
+++ testem.js
@@ -1,7 +1,12 @@
 /* eslint-env node */
+
+var MyReporter = require('./lib/my-reporter');
+
 module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
+  "reporter": new MyReporter(),
+  "report_file": "report.txt",
   "launch_in_ci": [
     "PhantomJS"
   ],
